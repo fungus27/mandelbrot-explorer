7ae0f060d20b4517255654fe28fb11cb4ce7390f
fungus <tomasz.sobkowicz.oliveira@gmail.com>
2022-07-15 23:20:40 +0000

    basic triangle rendering

--- main.c
+++ main.c
@@ -1,23 +1,135 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+unsigned int compile_shaders(const char *vert_filepath, const char *frag_filepath) {
+    FILE *vert_file = fopen(vert_filepath, "r");
+    if (!vert_file) {
+        int error = errno;
+        fprintf(stderr, "unable to load vertex shader at '%s'.\nerror: %s", vert_filepath, strerror(error));
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *frag_file = fopen(frag_filepath, "r");
+    if (!frag_file) {
+        int error = errno;
+        fprintf(stderr, "unable to load fragment shader at '%s'.\nerror: %s", frag_filepath, strerror(error));
+        exit(EXIT_FAILURE);
+    }
+
+    fseek(vert_file, 0, SEEK_END);
+    unsigned int vert_size = ftell(vert_file);
+    fseek(vert_file, 0, SEEK_SET);
+
+    fseek(frag_file, 0, SEEK_END);
+    unsigned int frag_size = ftell(frag_file);
+    fseek(frag_file, 0, SEEK_SET);
+
+    char *vert_content = malloc(vert_size + 1);
+    fread(vert_content, vert_size, 1, vert_file);
+    vert_content[vert_size] = 0;
+
+    char *frag_content = malloc(frag_size + 1);
+    fread(frag_content, frag_size, 1, frag_file);
+    frag_content[frag_size] = 0;
+
+    unsigned int vert = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vert, 1, (const char**)&vert_content, NULL);
+    glCompileShader(vert);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vert, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        glGetShaderInfoLog(vert, 512, NULL, infoLog);
+        fprintf(stderr, "vertex shader failed to compile.\nerror: %s\n", infoLog);
+        exit(EXIT_FAILURE);
+    }
+
+    unsigned int frag = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(frag, 1, (const char**)&frag_content, NULL);
+    glCompileShader(frag);
+
+    glGetShaderiv(frag, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(frag, 512, NULL, infoLog);
+        fprintf(stderr, "fragment shader failed to compile.\nerror: %s\n", infoLog);
+        exit(EXIT_FAILURE);
+    }
+    
+    free(vert_content);
+    free(frag_content);
+
+    unsigned int prog = glCreateProgram();
+
+    glAttachShader(prog, vert);
+    glAttachShader(prog, frag);
+
+    glLinkProgram(prog);
+
+    glGetProgramiv(prog, GL_LINK_STATUS, &success);
+    if(!success) {
+        glGetProgramInfoLog(prog, 512, NULL, infoLog);
+        fprintf(stderr, "shader program failed to link.\nerror: %s\n", infoLog);
+        exit(EXIT_FAILURE);
+    }
+
+    glDeleteShader(vert);
+    glDeleteShader(frag);
+
+    glUseProgram(prog);
+
+    return prog;
+}
 
 int main() {
     if (!glfwInit()) {
         const char *description;
         int code = glfwGetError(&description);
         fprintf(stderr, "glfw failed to initialize.\nerror (%d): %s\n", code, description);
+        exit(EXIT_FAILURE);
     }
     glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
     GLFWwindow* window = glfwCreateWindow(640, 480, "dev", NULL, NULL);
     glfwMakeContextCurrent(window);
 
     GLenum err; 
-    if ((err = glewInit()) != GLEW_OK)
+    if ((err = glewInit()) != GLEW_OK) {
         fprintf(stderr, "glew failed to initialize.\nerror (%u): %s\n", err, glewGetErrorString(err));
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("version: %s\n", glGetString(GL_VERSION));
 
+    float vertices[] = {
+        0.0f, 0.5f,
+        0.5f, -0.5f,
+        -0.5f, -0.5f
+    };
+    
+    unsigned int vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    unsigned int vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    compile_shaders("vert.glsl", "frag.glsl");
+    
     while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
